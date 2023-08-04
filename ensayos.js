7cf8c3c80d1762882ea29a3e784c1f94d98b099c
Egosorio92 <geoed13@gmail.com>
2023-08-03 21:42:11 -0500

    Agregar .gitignore y primeros archivos

---
+++ ensayos.js
@@ -0,0 +1,9 @@
+var a = prompt('Por favor, ingresa un número a:');
+var b = prompt('Por favor, ingresa un número b:');
+
+var funsuma = function(a,b){
+   var suma = a+b;
+   console.log(suma);
+}
+funsuma()
+
---
+++ valores.js
@@ -0,0 +1,12 @@
+var edad = 18;
+if (edad === 18){
+    console.log("puede votar");
+} else if (edad < 18){
+    console.log("no puede votar");
+} else {
+    console.log("siempre puede votar");
+}
+
+var nombre = "Edward";
+var  evaluar = nombre === "Edward" ? "Si es Edward": "No es Edward";
+console.log(evaluar)
